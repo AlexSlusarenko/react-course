620b70f5bb0c9214ecbc646166155192b1c263a9
OleksiiSliusarenko <slusarenkoa@gmail.com>
2019-09-02 22:31:05 +0300

    Backdrop is used for sidebar

--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -2,19 +2,27 @@ import React from 'react';
 
 import Logo from '../../Logo/Logo';
 import NavigationItems from '../Items/Items';
+import Aux from '../../../hoc/Auxiliary';
+import Backdrop from '../../UI/Backdrop/Backdrop';
 import classes from './SideDrawer.css';
 
 const sideDrawer = (props) => {
-
+  let usedClasses = [classes.SideDrawer, classes.Close];
+  if (props.open) {
+    usedClasses = [classes.SideDrawer, classes.Open];
+  }
   return (
-    <div className={classes.SideDrawer}>
-      <div className={classes.Logo}>
-        <Logo/>
+    <Aux>
+      <Backdrop show={props.open} clicked={props.closed}/>
+      <div className={usedClasses.join(' ')}>
+        <div className={classes.Logo}>
+          <Logo/>
+        </div>
+        <nav>
+          <NavigationItems/>
+        </nav>
       </div>
-      <nav>
-        <NavigationItems/>
-      </nav>
-    </div>
+    </Aux>
   );
 };
 
